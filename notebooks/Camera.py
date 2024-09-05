2d495e7b9ccaa200870d3e0d5f18f7962e0ac06a
DavidTung-TRI <david.tung@tri.global>
2024-09-05 15:40:50 -0700

    update camera

--- notebooks/Camera.py
+++ notebooks/Camera.py
@@ -1,18 +1,28 @@
-from ultralytics import YOLOWorld
+import cv2
+
 
 class Camera:
     
     def __init__(self):
         self.model = None
-    
-    def predict(self):
-        pass
 
+OUT_FILE = "/tmp/tmp2.jpg"
+RANGE = 5
     
-class MonoCamera(Camera):
+    
+class MonocularCamera(Camera): 
     
     def __init__(self):
         pass
+
+    def extractImage(self, video_file):
+        cam = cv2.VideoCapture(video_file)
+        for i in range(0, RANGE):
+            ret, frame = cam.read()  # read 1st image
+        cv2.imwrite(OUT_FILE, frame)
+        cam.release()
+        return OUT_FILE
+
         
     
     
